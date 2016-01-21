1f9e8191df43d11b3bb133141a62b73698b6aee9
Mehak <mehak@gems.techverx.com>
2016-01-21 16:47:16 +0500

    commiting font awesome

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module CatalogNew
   class Application < Rails::Application
        config.assets.enabled = true
        config.assets.paths << "#{Rails.root}/app/assets/fonts" 
+       config.assets.precompile += %w( .svg .eot .woff .ttf )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
