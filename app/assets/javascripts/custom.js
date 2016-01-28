aaea078e395d4358f1be1ff8578ed99fb3f6bdec
Mehak <mehak@gems.techverx.com>
2016-01-28 16:45:07 +0500

    Flip BOOk

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -19,15 +19,23 @@ $(".signin-txt").click(function(){
     $(".btn-signin").show();
 });
 
-$(".modalclose").click(function(){
+$(".modalclose").click(function(){    
     $(".box-forgot").hide();
     $(".btn-forgot").hide();
     $("#myModalLabel").text('Sign In')
     $(".box-signin").show();
     $(".btn-signin").show();
+    $(".parsley-required").html("");   
+    $(".parsley-type").html("");   
+    $(".parsley-minlength").html("");      
+    $(".parsley-equalto").html("");   
+    $(".parsley-pattern ").html("");   
+    $('.form-control').removeClass('parsley-error');
+    $('.form-control').removeClass('parsley-success');    
+    $("input[type=text], textarea").val("");
+    $("input[type=password]").val("");
+    $("input[type=email]").val("");
 });
-
-
 
 $(".signin-go").click(function(event){
 	event.preventDefault();
@@ -75,7 +83,23 @@ $(window).load(function() {
   });
 });
 
+//$('body').on('hidden.bs.modal', '.modal', function () {
+//        $(this).removeData('createAccount');
+//        alert("hogya");
+//      });
 
+ $(function(){   
+      $('#createAccount').on('hidden.bs.modal', function () {
+        console.log("in funct");
+         $(this).removeData();
+          alert('hidden event fired!');
+      });
+
+       $('#createAccount').on('shown.bs.modal', function () {
+          console.log("in funct show");
+          alert('show event fired!');
+      });
+});
 
 /* Window.Load Ends */
 
