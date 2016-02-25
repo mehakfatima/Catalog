895251a4abef7fdb79bca67cc6524e1be48b6479
aqsaisrar <aqsa@gems.techverx.com>
2016-02-25 13:56:17 +0500

    added organization to categories and added back button

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,5 +7,11 @@ class Category < ActiveRecord::Base
   
   accepts_nested_attributes_for :product_categories
   accepts_nested_attributes_for :products
+
+  def organization_name
+    if self.organization.present?
+      organization.name
+    end
+  end
 end
 
--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,6 +1,6 @@
 class ProductCategory < ActiveRecord::Base
-  belongs_to :product
-  belongs_to :category 
+  belongs_to :product, dependent: :destroy
+  belongs_to :category, dependent: :destroy
   accepts_nested_attributes_for :product,
     :allow_destroy => true
   accepts_nested_attributes_for :category,
